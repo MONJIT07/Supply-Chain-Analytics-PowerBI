77c1c95ff9e4e9ab8812f849898e08ead0047a31
MONJIT07 <monjittamuli7747@gmail.com>
2026-08-24 17:19:28 +0530

    Delete backend directory

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getProducts, createProduct, deleteProduct } from '../controllers/productController.js'
-
-const router = express.Router()
-
-router.route('/').get(getProducts).post(createProduct)
-router.route('/:id').delete(deleteProduct)
-
-export default router
